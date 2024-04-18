f59fad406be4cc9abc29d52a097d9d204aa12d25
Nikita <nikitaefron06@gmail.com>
2024-04-18 23:15:31 +0200

    finish Task

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1",
+  baseURL: "https://api.themoviedb.org/3/",
   headers: {
     Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYjVmZGRlYzA0ZjZkMzg0ZWJkOWY3ODllOGZjZjlhNCIsInN1YiI6IjY2MjE3YWFjZTRiNTc2MDE0YWJiYzk1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eMu-wFNbkwYM_UrzLCQrdhtt-igdZPqjQEh-bA_3-MU`,
   },
